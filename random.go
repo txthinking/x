f5681c925804b2d7d882b76814a5e4eaaa440490
Cloud <cloud@txthinking.com>
2017-06-15 08:21:17 +0000

    randome number

--- random.go
+++ random.go
@@ -10,3 +10,9 @@ func RandomNumber() (i int64) {
 	i = rand.New(rand.NewSource(time.Now().UnixNano())).Int63()
 	return
 }
+
+// Random used to get a random number between min and max
+func Random(min, max int64) int64 {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Int63n(max-min) + min
+}
--- random_test.go
+++ random_test.go
@@ -8,4 +8,6 @@ func TestRandomNumber(t *testing.T) {
 	t.Log(RandomNumber())
 	t.Log(RandomNumber())
 	t.Log(RandomNumber())
+	t.Log(Random(1000, 9999))
+	t.Log(Random(1000, 9999))
 }
