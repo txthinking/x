3a0f2bf5d06c110b5a66aead3e4594c5cc00e605
Cloud <cloud@txthinking.com>
2017-07-28 04:10:00 +0000

    fixed bug

--- notification_test.go
+++ notification_test.go
@@ -3,10 +3,10 @@ package ant
 import "testing"
 
 func TestSlack(t *testing.T) {
-	s := &Slack{
-		URL:     "https://hooks.slack.com/services/xx",
-		IconURL: "https://txthinking-file.storage.googleapis.com/a1d234ca87944bf7b4f1e8c8d06e4474/NoName.jpeg",
-	}
+	//s := &Slack{
+	//URL:     "https://hooks.slack.com/services/xx",
+	//IconURL: "https://txthinking-file.storage.googleapis.com/a1d234ca87944bf7b4f1e8c8d06e4474/NoName.jpeg",
+	//}
 	//if err := s.Send("hello bot"); err != nil {
 	//t.Fatal(err)
 	//}
