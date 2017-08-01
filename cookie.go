bcef58ba0704eb6f9f508002720ffe7ad388ef27
Cloud <cloud@txthinking.com>
2017-08-01 09:13:11 +0000

    add crypt kv

---
+++ cookie.go
@@ -0,0 +1,69 @@
+package ant
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+// CryptKV can be used to crypt value by key and give it a lifecycle (s).
+// Can be used in cookies
+type CryptKV struct {
+	AESKey    []byte
+	LifeCycle int
+}
+
+// Encrypt key, value
+func (kv *CryptKV) Encrypt(k string, v string) (string, error) {
+	m := map[string]interface{}{
+		"k":          k,
+		"v":          v,
+		"expired_at": time.Now().Add(time.Second * time.Duration(kv.LifeCycle)).Unix(),
+	}
+	d, err := json.Marshal(m)
+	if err != nil {
+		return "", err
+	}
+	d, err = AESCFBEncrypt(d, AESMake256Key(kv.AESKey))
+	if err != nil {
+		return "", err
+	}
+	return url.PathEscape(string(d)), nil
+}
+
+// Decrypt key, value
+func (kv *CryptKV) Decrypt(c string, k string) (string, error) {
+	s, err := url.PathUnescape(c)
+	if err != nil {
+		return "", err
+	}
+	m := make(map[string]interface{})
+	d, err := AESCFBDecrypt([]byte(s), AESMake256Key(kv.AESKey))
+	if err != nil {
+		return "", err
+	}
+	if err := json.Unmarshal(d, &m); err != nil {
+		return "", err
+	}
+	if time.Unix(int64(m["expired_at"].(float64)), 0).Before(time.Now()) {
+		return "", errors.New("Expired")
+	}
+	if m["k"].(string) != k {
+		return "", errors.New("Unmarch key")
+	}
+	return m["v"].(string), nil
+}
+
+func (kv *CryptKV) GetIDFromRequest(r *http.Request, k string) (string, error) {
+	c, err := r.Cookie(k)
+	if err != nil {
+		return "", err
+	}
+	v, err := kv.Decrypt(c.Value, k)
+	if err != nil {
+		return "", err
+	}
+	return v, nil
+}
---
+++ cookie_test.go
@@ -0,0 +1,23 @@
+package ant
+
+import "testing"
+
+func TestCryptKV(t *testing.T) {
+	kv := &CryptKV{
+		LifeCycle: 7 * 24 * 60 * 60,
+		AESKey:    []byte("qwertyuiopasdfghjklzqwertyuiopqw"),
+	}
+	k := "t"
+
+	v := "hello"
+	c, err := kv.Encrypt(k, v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(c)
+	v1, err := kv.Decrypt(c, k)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(v1)
+}
