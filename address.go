ad782b0ecf0d8f7f571752f2012d2cdbc4a60ff1
Cloud <cloud@txthinking.com>
2018-03-01 19:29:31 +0800

    url

--- address.go
+++ address.go
@@ -1,23 +1,23 @@
 package ant
 
 import (
-	"net"
-	"net/url"
-	"regexp"
+    "net"
+    "net/url"
+    "regexp"
 )
 
 func GetAddressFromURL(s string) (string, error) {
-	u, err := url.Parse(s)
-	if err != nil {
-		return "", err
-	}
-	if HasPort(u.Host) {
-		return u.Host, nil
-	}
-	return net.JoinHostPort(u.Host, "80"), nil
+    u, err := url.Parse(s)
+    if err != nil {
+        return "", err
+    }
+    if HasPort(u.Host) {
+        return u.Host, nil
+    }
+    return net.JoinHostPort(u.Host, "80"), nil
 }
 
 func HasPort(host string) bool {
-	r := regexp.MustCompile(`.+:\d+$`)
-	return r.MatchString(host)
+    r := regexp.MustCompile(`.+:\d+$`)
+    return r.MatchString(host)
 }
