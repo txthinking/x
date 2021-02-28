fb6650fe490a76f18023076016434ac37bcdb6f6
idk <hankhill19580@gmail.com>
2021-02-28 17:02:56 -0500

    use interfaces for  Dialer functions so that I can implement a SOCKS proxy for I2P

--- dial.go
+++ dial.go
@@ -7,8 +7,8 @@ import (
 // Dialer is a common interface for dialing
 type Dialer interface {
 	Dial(network, addr string) (net.Conn, error)
-	DialTCP(network string, laddr, raddr *net.TCPAddr) (*net.TCPConn, error)
-	DialUDP(network string, laddr, raddr *net.UDPAddr) (*net.UDPConn, error)
+	DialTCP(network string, laddr, raddr net.Addr) (net.Conn, error)
+	DialUDP(network string, laddr, raddr net.Addr) (net.PacketConn, error)
 }
 
 type Dial struct {
@@ -21,10 +21,10 @@ func (d *Dial) Dial(network, addr string) (net.Conn, error) {
 	return net.Dial(network, addr)
 }
 
-func (d *Dial) DialTCP(network string, laddr, raddr *net.TCPAddr) (*net.TCPConn, error) {
-	return net.DialTCP(network, laddr, raddr)
+func (d *Dial) DialTCP(network string, laddr, raddr net.Addr) (net.Conn, error) {
+	return net.DialTCP(network, laddr.(*net.TCPAddr), raddr.(*net.TCPAddr))
 }
 
-func (d *Dial) DialUDP(network string, laddr, raddr *net.UDPAddr) (*net.UDPConn, error) {
-	return net.DialUDP(network, laddr, raddr)
+func (d *Dial) DialUDP(network string, laddr, raddr net.Addr) (net.PacketConn, error) {
+	return net.DialUDP(network, laddr.(*net.UDPAddr), raddr.(*net.UDPAddr))
 }
